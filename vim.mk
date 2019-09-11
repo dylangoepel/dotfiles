4809bc05bd1eeb66ea84863ad3ce5c3e1c4f8a1b
Dylan Göpel <>
2019-09-11 18:08:57 +0200

    .

--- vim.mk
+++ vim.mk
@@ -3,7 +3,7 @@ vim: /usr/bin/nvim ~/.config/nvim/init.vim
 /usr/bin/nvim:
 	$(install) neovim
 
-~/.config/nvim/init.vim: vim/init.vim ~/.config/nvim/init.vim ~/.local/share/nvim/site/autoload/plug.vim /usr/bin/python3
+~/.config/nvim/init.vim: vim/init.vim  ~/.local/share/nvim/site/autoload/plug.vim /usr/bin/python3
 	sudo pip3 install pynvim
 	mkdir -p ~/.config/nvim
 	cp vim/init.vim ~/.config/nvim/init.vim
