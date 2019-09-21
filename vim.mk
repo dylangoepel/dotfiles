f7c47a436c10f5dcf280c9a63a1db8052a9d4702
dylangoepel <dylan@chaospott.de>
2019-09-21 11:21:00 +0200

    install pynvim only if required.

--- vim.mk
+++ vim.mk
@@ -1,10 +1,12 @@
+pynvim=/usr/lib/python3.7/site-packages/pynvim/__init__.py
+python=/usr/bin/python3
+
 vim: /usr/bin/nvim ~/.config/nvim/init.vim
 
 /usr/bin/nvim:
 	$(install) neovim
 
-~/.config/nvim/init.vim: vim/init.vim  ~/.local/share/nvim/site/autoload/plug.vim /usr/bin/python3
-	sudo pip3 install pynvim
+~/.config/nvim/init.vim: vim/init.vim  ~/.local/share/nvim/site/autoload/plug.vim $(python) $(pynvim)
 	mkdir -p ~/.config/nvim
 	cp vim/init.vim ~/.config/nvim/init.vim
 
@@ -12,3 +14,6 @@ vim: /usr/bin/nvim ~/.config/nvim/init.vim
 ~/.local/share/nvim/site/autoload/plug.vim: 
 	curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
 	    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+$(pynvim):
+	sudo pip3 install pynvim
