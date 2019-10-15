f97be9a82ea9a13be4e4d9a6d630fbd26775b767
dylangoepel <dylan@chaospott.de>
2019-10-15 21:34:04 +0200

    .

--- de.mk
+++ de.mk
@@ -1,6 +1,6 @@
-de: xorg i3 startx
+de: xorg i3 startx nitrogen sddm compton
 
-xorg: /usr/bin/Xorg
+xorg: /usr/bin/Xorg /etc/X11/xorg.conf.d/xorg.conf
 /usr/bin/Xorg:
 	$(install) xorg
 
@@ -18,3 +18,22 @@ i3: /usr/bin/i3 ~/.config/i3/config
 ~/.config/i3/config: i3/config
 	mkdir -p ~/.config/i3/
 	cp i3/config $@
+
+nitrogen: /usr/bin/nitrogen
+/usr/bin/nitrogen:
+	$(install) nitrogen
+
+sddm: /usr/bin/sddm /usr/lib/sddm/sddm.conf.d/default.conf /etc/systemd/system/display-manager.service
+/usr/bin/sddm:
+	$(install) sddm
+	localectl set-x11-keymap de
+
+/usr/lib/sddm/sddm.conf.d/default.conf: xorg/sddm.conf
+	sudo cp $< $@
+
+/etc/systemd/system/display-manager.service:
+	sudo systemctl enable sddm
+
+compton: /usr/bin/compton
+/usr/bin/compton:
+	$(install) compton
