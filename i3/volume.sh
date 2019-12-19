e9546fb464b343e04080e81fe82a010c3213d11e
Dylan Göpel <>
2019-12-19 19:12:14 +0100

    new features

--- i3/volume.sh
+++ i3/volume.sh
@@ -3,6 +3,21 @@ set ${1:-get}
  
 sinks=$(pacmd list-sinks | grep index | sed "s,.*index: ,,g")
 
+if [[ $1 == "cycle" ]]
+then
+    current=$(pactl list sink-inputs | grep "Sink:" | head -n 1 | tr -d "\t" | sed "s,Sink: ,,g")
+    lastSink=$(echo $sinks | grep -Eo "[0-9]+ {0,1}$")
+
+    new=$(( ($current + 1) % ($lastSink + 1) ))
+
+    inputs=$(pactl list sink-inputs | grep "Sink Input #" | sed "s,Sink Input #,,g")
+
+    for input in $inputs
+    do
+        pactl move-sink-input $input $new
+    done
+fi
+
 for sink in $sinks
 do
     if [[ $1 == "inc" ]]
