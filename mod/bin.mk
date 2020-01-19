6b5445177d54beeb608a3b5929ffe75c30f69491
Dylan Göpel <>
2020-01-19 15:20:06 +0100

    .

--- mod/bin.mk
+++ mod/bin.mk
@@ -1,17 +1,5 @@
-bin: /usr/bin/volume /usr/bin/productivity /bin/vim /usr/bin/dl
+bin: /bin/volume /bin/productivity /bin/vim /bin/dl /bin/bib /bin/med
 
-/usr/bin/volume: res/bin/volume.sh
-	sudo cp res/bin/volume.sh /usr/bin/volume
-	sudo chmod +x /usr/bin/volume
-
-/usr/bin/productivity: res/bin/productivity.sh
-	sudo cp res/bin/productivity.sh /usr/bin/productivity
-	sudo chmod +x /usr/bin/productivity
-
-/bin/vim: res/bin/vim
-	sudo cp res/bin/vim /bin/vim
-	sudo chmod +x /bin/vim
-
-/usr/bin/dl: res/bin/dl 
-	sudo cp res/bin/dl /usr/bin/dl
-	sudo chmod +x /usr/bin/dl
+/bin/%: res/bin/%.sh
+	sudo cp $< $@
+	sudo chmod +x $@
