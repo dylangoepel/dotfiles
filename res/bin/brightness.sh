71bcc311ea14ef161317a42ed708d2fc2740ca73
Dylan Göpel <>
2020-03-20 13:10:16 +0100

    Use light instead of xbacklight in brightness.sh

--- res/bin/brightness.sh
+++ res/bin/brightness.sh
@@ -9,7 +9,7 @@
 # https://gist.github.com/sebastiencs/5d7227f388d93374cebdf72e783fbd6a
 
 function get_brightness {
-  xbacklight -get | cut -d '.' -f 1
+    light -G
 }
 
 function send_notification {
@@ -18,26 +18,28 @@ function send_notification {
   # Make the bar with the special character ─ (it's not dash -)
   # https://en.wikipedia.org/wiki/Box-drawing_character
   # Send the notification
-  dunstify -a "Brightness" -i "$icon" -r 5555 -u normal "Updated to $brightness"
+  dunstify -a "Brightness" -i "$icon" -r 5555 -u normal "Updated to $brightness%"
 }
 
 case $1 in
   up)
     # increase the backlight by 5%
-    xbacklight -inc 5
+    light -A 5
     send_notification
     ;;
   down)
     # decrease the backlight by 5%
-    xbacklight -dec 5
+    light -U 5
     send_notification
     ;;
   high)
     # set brightness to a predefined high level
-    xbacklight -set 60
+    light -S 60
+    send_notification
     ;;
   low)
     # set brightness to a predefined low level
-    xbacklight -set 10
+    light -S 5
+    send_notification
     ;;
 esac
